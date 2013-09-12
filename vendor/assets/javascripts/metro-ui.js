b5cade491a19e3f8c43e52993a211317516eccbb
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2013-09-12 10:28:45 -0300

    Back to old

--- vendor/assets/javascripts/metro-ui.js
+++ vendor/assets/javascripts/metro-ui.js
@@ -1,9 +1,11 @@
+//= require jquery.mousewheel.min.js
 //= require metro-ui/pagecontrol
 //= require metro-ui/accordion
 //= require metro-ui/calendar
 //= require metro-ui/dropdown
 //= require metro-ui/start-menu
 //= require metro-ui/dialog
+//= require metro-ui/tile-drag
 //= require metro-ui/tile-slider
 //= require metro-ui/carousel
 //= require metro-ui/buttonset
